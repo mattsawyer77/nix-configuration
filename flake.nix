a68a7a39dc5e8921be4e90e3f7f4293f030ee83c
Matt Sawyer <m.sawyer@f5.com>
2022-04-27 14:27:24 -0700

    simplify emacs overlay and roll it back to a known good version for emacs 29

--- flake.nix
+++ flake.nix
@@ -7,35 +7,12 @@
       inputs.nixpkgs.follows = "unstable";
     };
     emacs-overlay = {
-      url = "github:nix-community/emacs-overlay";
-      inputs.nixpkgs.follows = "unstable";
+      url =
+        "github:nix-community/emacs-overlay/58036d5e198e8595e3d9637e0e918aa31ac7dfdf";
     };
     neovim-nightly-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "unstable";
-    };
-    # doom-emacs = {
-    #   url = "github:hlissner/doom-emacs/master";
-    #   flake = false;
-    # };
-    # nix-straight = {
-    #   url = "github:nix-community/nix-straight.el";
-    #   flake = false;
-    # };
-    # nix-doom-emacs = {
-    #   url = "github:nix-community/nix-doom-emacs";
-    #   inputs.nixpkgs.follows = "unstable";
-    #   inputs.doom-emacs.follows = "doom-emacs";
-    #   inputs.nix-straight.follows = "nix-straight";
-    # };
-    emacs-src = {
-      url = "github:emacs-mirror/emacs";
-      flake = false;
-    };
-    # Use latest libverm to build macOS emacs build
-    emacs-vterm-src = {
-      url = "github:akermu/emacs-libvterm";
-      flake = false;
     };
   };
   outputs = { self, nixpkgs, darwin, ... }@inputs: {
--- modules/mac.nix
+++ modules/mac.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, emacs-src, emacs-vterm-src, neovim-nightly-overlay, ... }:
+{ config, pkgs, lib, emacs-overlay, neovim-nightly-overlay, ... }:
 
 with lib;
 
@@ -36,7 +36,7 @@ let
     diff-so-fancy
     direnv
     dos2unix
-    emacs-mac
+    emacsGit # from emacs-overlay
     etcd
     eternal-terminal
     exa
@@ -391,73 +391,7 @@ in {
     overlays = [
       (import ./neovim.nix)
       (import ./golangci-lint.nix)
-      (final: prev: {
-        emacs-vterm = prev.stdenv.mkDerivation rec {
-          pname = "emacs-vterm";
-          version = "master";
-
-          src = emacs-vterm-src;
-
-          nativeBuildInputs = [ prev.cmake prev.libtool prev.glib.dev ];
-
-          buildInputs = [ prev.glib.out prev.libvterm-neovim prev.ncurses ];
-
-          cmakeFlags = [ "-DUSE_SYSTEM_LIBVTERM=yes" ];
-
-          preConfigure = ''
-            echo "include_directories(\"${prev.glib.out}/lib/glib-2.0/include\")" >> CMakeLists.txt
-            echo "include_directories(\"${prev.glib.dev}/include/glib-2.0\")" >> CMakeLists.txt
-            echo "include_directories(\"${prev.ncurses.dev}/include\")" >> CMakeLists.txt
-            echo "include_directories(\"${prev.libvterm-neovim}/include\")" >> CMakeLists.txt
-          '';
-
-          installPhase = ''
-            mkdir -p $out
-            cp ../vterm-module.so $out
-            cp ../vterm.el $out
-          '';
-
-        };
-        emacs-mac = (prev.emacs.override {
-          srcRepo = true;
-          nativeComp = true;
-          withSQLite3 = true;
-          withXwidgets = true;
-        }).overrideAttrs (o: rec {
-          version = "29.0.50";
-          src = emacs-src;
-
-          buildInputs = o.buildInputs
-            ++ [ prev.darwin.apple_sdk.frameworks.WebKit ];
-
-          configureFlags = o.configureFlags ++ [
-            "--without-gpm"
-            "--without-dbus"
-            "--without-mailutils"
-            "--without-toolkit-scroll-bars"
-            "--without-pop"
-          ];
-
-          patches = [ ../patches/fix-window-role.patch ]
-            ++ (if pkgs.stdenv.isAarch64 then
-              [ ../patches/system-appearance.patch ]
-            else
-              [ ]);
-
-          postPatch = o.postPatch + ''
-            substituteInPlace lisp/loadup.el \
-            --replace '(emacs-repository-get-branch)' '"master"'
-          '';
-
-          postInstall = o.postInstall + ''
-            cp ${final.emacs-vterm}/vterm.el $out/share/emacs/site-lisp/vterm.el
-            cp ${final.emacs-vterm}/vterm-module.so $out/share/emacs/site-lisp/vterm-module.so
-          '';
-
-          CFLAGS =
-            "-DMAC_OS_X_VERSION_MAX_ALLOWED=110203 -g -O3 -mtune=native -march=native -fomit-frame-pointer";
-        }); # emacs-mac
-      }) # inline overlays
+      emacs-overlay.overlay
     ]; # overlays
   }; # nixpkgs
 }
