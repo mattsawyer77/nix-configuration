26bb80455329562ac7be9f60738046d6ccde63f4
Matt Sawyer <m.sawyer@f5.com>
2023-01-27 09:04:20 -0800

    nixos: switch to custom emacs overlay to get vterm working

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -1,4 +1,12 @@
-{ config, pkgs, lib, emacs-overlay, neovim-nightly-overlay, ... }:
+{ config
+, pkgs
+, lib
+, emacs-overlay
+, emacs-src
+, emacs-vterm-src
+, neovim-nightly-overlay
+, ...
+}:
 
 with lib;
 
@@ -27,7 +35,9 @@ with lib;
     docker
     docker-compose
     dos2unix
-    emacsGit-nox
+    # emacsGit-nox
+    emacs-nixos # from overlay below
+    emacs-vterm
     envsubst
     etcd
     eternal-terminal
@@ -153,7 +163,58 @@ with lib;
   };
   nixpkgs = {
     config.allowUnfree = true;
-    overlays = [ emacs-overlay.overlay (import ./neovim.nix) ]; # overlays
+    overlays = [
+      emacs-overlay.overlay
+      (import ./neovim.nix)
+      (final: prev: {
+        emacs-vterm = prev.stdenv.mkDerivation rec {
+          pname = "emacs-vterm";
+          version = "master";
+          src = emacs-vterm-src;
+          nativeBuildInputs = [ prev.cmake prev.libtool prev.glib.dev ];
+          buildInputs = [ prev.glib.out prev.libvterm-neovim prev.ncurses ];
+          cmakeFlags = [ "-DUSE_SYSTEM_LIBVTERM=yes" ];
+          preConfigure = ''
+            echo "include_directories(\"${prev.glib.out}/lib/glib-2.0/include\")" >> CMakeLists.txt
+            echo "include_directories(\"${prev.glib.dev}/include/glib-2.0\")" >> CMakeLists.txt
+            echo "include_directories(\"${prev.ncurses.dev}/include\")" >> CMakeLists.txt
+            echo "include_directories(\"${prev.libvterm-neovim}/include\")" >> CMakeLists.txt
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp ../vterm-module.so $out
+            cp ../vterm.el $out
+          '';
+        };
+        emacs-nixos = (prev.emacsGit.override {
+          srcRepo = true;
+          nativeComp = true;
+          withSQLite3 = true;
+        }).overrideAttrs (o: rec {
+          version = "30.0.50";
+          src = emacs-src;
+          buildInputs = o.buildInputs;
+          configureFlags = o.configureFlags ++ [
+            "--with-modules"
+            "--without-gpm"
+            "--without-dbus"
+            "--without-mailutils"
+            "--without-pop"
+          ];
+          postPatch = o.postPatch + ''
+            substituteInPlace lisp/loadup.el \
+            --replace '(emacs-repository-get-branch)' '"master"'
+          '';
+          postInstall = o.postInstall + ''
+            cp ${final.emacs-vterm}/vterm.el $out/share/emacs/site-lisp/vterm.el
+            cp ${final.emacs-vterm}/vterm-module.so $out/share/emacs/site-lisp/vterm-module.so
+          '';
+          CFLAGS =
+            # "-DMAC_OS_X_VERSION_MAX_ALLOWED=110203 -g -O3 -mtune=native -march=native -fomit-frame-pointer";
+            "-g -O3 -mtune=native -march=native -fomit-frame-pointer";
+        });
+      })
+    ]; # overlays
   }; # nixpkgs
   # programs.direnv.enable = true;
   # programs.direnv.nix-direnv.enable = true;
