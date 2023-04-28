99536cc7923a295b9dfb5023f18d7aad5af2dabe
Matt Sawyer <236582+mattsawyer77@users.noreply.github.com>
2023-04-27 17:52:19 -0700

    move emacs build to its own module

--- modules/mac.nix
+++ modules/mac.nix
@@ -72,64 +72,12 @@ in
     };
     overlays = [
       emacs-overlay.overlay
+      (import ./emacs-mac.nix {
+        inherit config pkgs lib emacs-overlay emacs-src emacs-vterm-src;
+      })
       (import ./neovim.nix)
       nil.overlays.nil
       (import ./golangci-lint.nix)
-      # TODO: figure out how to move the following to a separate file
-      (final: prev: {
-        emacs-vterm = prev.stdenv.mkDerivation rec {
-          pname = "emacs-vterm";
-          version = "master";
-          src = emacs-vterm-src;
-          nativeBuildInputs = [ prev.cmake prev.libtool prev.glib.dev ];
-          buildInputs = [ prev.glib.out prev.libvterm-neovim prev.ncurses ];
-          cmakeFlags = [ "-DUSE_SYSTEM_LIBVTERM=yes" ];
-          preConfigure = ''
-            echo "include_directories(\"${prev.glib.out}/lib/glib-2.0/include\")" >> CMakeLists.txt
-            echo "include_directories(\"${prev.glib.dev}/include/glib-2.0\")" >> CMakeLists.txt
-            echo "include_directories(\"${prev.ncurses.dev}/include\")" >> CMakeLists.txt
-            echo "include_directories(\"${prev.libvterm-neovim}/include\")" >> CMakeLists.txt
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp ../vterm-module.so $out
-            cp ../vterm.el $out
-          '';
-        };
-        emacs-mac = (prev.emacsGit.override {
-          srcRepo = true;
-          nativeComp = true;
-          withSQLite3 = true;
-          # withXwidgets = true;
-        }).overrideAttrs (o: rec {
-          version = "30.0.50";
-          src = emacs-src;
-          buildInputs = o.buildInputs ++ [ prev.darwin.apple_sdk.frameworks.WebKit ];
-          configureFlags = o.configureFlags ++ [
-            "--with-modules"
-            "--without-gpm"
-            "--without-dbus"
-            "--without-mailutils"
-            "--with-toolkit-scroll-bars"
-            "--without-pop"
-          ];
-          patches = [
-            ../patches/fix-window-role.patch
-            # ./patches/system-appearance.patch
-          ];
-          postPatch = o.postPatch + ''
-            substituteInPlace lisp/loadup.el \
-            --replace '(emacs-repository-get-branch)' '"master"'
-          '';
-          postInstall = o.postInstall + ''
-            cp ${final.emacs-vterm}/vterm.el $out/share/emacs/site-lisp/vterm.el
-            cp ${final.emacs-vterm}/vterm-module.so $out/share/emacs/site-lisp/vterm-module.so
-          '';
-          CFLAGS =
-            # "-DMAC_OS_X_VERSION_MAX_ALLOWED=110203 -g -O3 -mtune=native -march=native -fomit-frame-pointer";
-            "-g -O3 -mtune=native -march=native -fomit-frame-pointer";
-        });
-      })
     ]; # overlays
   }; # nixpkgs
 }
