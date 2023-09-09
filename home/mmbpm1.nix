46b845e64c506459ccac0f0f898514df49d5f161
Matt Sawyer <236582+mattsawyer77@users.noreply.github.com>
2023-09-09 16:40:44 -0700

    update inputs and fix zsh syntax highlighting var

--- home/mmbpm1.nix
+++ home/mmbpm1.nix
@@ -379,7 +379,7 @@ in
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     defaultKeymap = "viins";
     history = {
       size = 100000;
