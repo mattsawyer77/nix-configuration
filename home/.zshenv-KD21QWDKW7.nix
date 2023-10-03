e389a77aee020c0756ecf4868c558fa41d4e02d1
Matt Sawyer <matt.sawyer@gmail.com>
2023-10-03 07:42:10 -0700

    shell function to check 24-bit color

--- home/.zshenv-KD21QWDKW7.nix
+++ home/.zshenv-KD21QWDKW7.nix
@@ -14,6 +14,22 @@ nix-config-update() {
     && nix flake update)
 }
 
+check-color() {
+  awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+        r = 255-(colnum*255/76);
+        g = (colnum*510/76);
+        b = (colnum*255/76);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+  }'
+}
+
 launchctl-restart() {
   if [[ -v 1 ]]; then
     pattern="$1"
@@ -419,7 +435,6 @@ docker-shell () {
         --env GOCACHE=${go_cache_dir} \
         --env GOPATH=${GOPATH} \
         --env HOME=${HOME} \
-        --env TERM=xterm-256color \
         --env PS1="${image}:\w> " \
         --env DOCKER_IMAGE="$image" \
         --net host \
