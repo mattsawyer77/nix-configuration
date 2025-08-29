1939605dcab9ee3fee6ac201fbff8ed9588ee4c6
Matt Sawyer <matt.sawyer@gmail.com>
2025-08-29 10:10:10 -0700

    flake update

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -1,19 +1,36 @@
 { pkgs
-, ghosttyPackage ? pkgs.ghostty
+# , ghosttyPackage ? pkgs.ghostty
+  , configDir
 , ...
 }: {
-  home.packages = [ ghosttyPackage ];
-  programs.ghostty = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-      theme = "Kanagawa Dragon";
-      font-size = 21;
-      font-family = "PragmataPro Liga";
-      # keybind = [
-      #   "ctrl+h=goto_split:left"
-      #   "ctrl+l=goto_split:right"
-      # ];
-    };
+  # XXX: ghostty marked as broken on macOS as of 2025-08-11
+  # home.packages = [ ghosttyPackage ];
+  # programs.ghostty = {
+  #   enable = true;
+  #   enableZshIntegration = true;
+  #   settings = {
+  #     font-size = 21;
+  #     font-family = "PragmataPro Liga";
+  #     # theme = "dark:Kanagawa Wave,light:ayu_light"
+  #     # theme = "dark:Ghostty Default StyleDark,light:ayu_light"
+  #     # theme = "dark:Argonaut,light:ayu_light"
+  #     theme = "dark:kanagawabones,light:ayu_light";
+  #     window-padding-x = "10,10";
+  #     window-padding-y = "10,10";
+  #     adjust-cell-height = "15%";
+  #     adjust-font-baseline = 2;
+  #     cursor-style = "block";
+  #     # keybind = [
+  #     #   "ctrl+h=goto_split:left"
+  #     #   "ctrl+l=goto_split:right"
+  #     # ];
+  #   };
+  # };
+
+  # for now, just copy the ghostty config file into place
+  home.file."ghostty-config" = {
+    source = ./config;
+    target = "${configDir}/config";
+    force = true;
   };
 }
