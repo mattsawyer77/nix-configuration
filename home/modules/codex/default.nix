7148d0902c27f328f31ff1544a66fa6af80dc802
Matt Sawyer <m.sawyer@f5.com>
2026-04-09 12:00:28 -0700

    codex update before refactor

--- home/modules/codex/default.nix
+++ home/modules/codex/default.nix
@@ -11,14 +11,19 @@
       target = "${config.home.homeDirectory}/.codex/config.toml";
       force = true;
     };
-    home.file."codex-worker-gpt53-agent" = {
-      source = ./agents/worker-gpt53-codex.toml;
-      target = "${config.home.homeDirectory}/.codex/agents/worker-gpt53-codex.toml";
+    # home.file."codex-worker-gpt53-agent" = {
+    #   source = ./agents/worker-gpt53-codex.toml;
+    #   target = "${config.home.homeDirectory}/.codex/agents/worker.toml";
+    #   force = true;
+    # };
+    home.file."codex-worker-gemma4-agent" = {
+      source = ./agents/worker-gemma-4-26b.toml;
+      target = "${config.home.homeDirectory}/.codex/agents/worker.toml";
       force = true;
     };
     home.file."codex-explorer-gpt54-agent" = {
       source = ./agents/explorer-gpt54-codex.toml;
-      target = "${config.home.homeDirectory}/.codex/agents/explorer-gpt54-codex.toml";
+      target = "${config.home.homeDirectory}/.codex/agents/explorer.toml";
       force = true;
     };
     home.activation = {
